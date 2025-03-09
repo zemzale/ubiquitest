7f8bb7da76a661b2613de3f97abbfd8b9a4639b8
Alberts Zemzale <alberts.zemzale@gmail.com>
2025-03-09 20:42:22 +0200

    feat: Broadcast to all users other than the the originator

--- server/router/router.go
+++ server/router/router.go
@@ -133,11 +133,19 @@ var upgrader = websocket.Upgrader{
 }
 
 func (r *Router) WsTodos(writer http.ResponseWriter, request *http.Request) {
+	username := request.URL.Query().Get("user")
+	if username == "" {
+		log.Println("No user name provided")
+		return
+	}
+
+	log.Println("User name:", username)
+
 	conn, err := upgrader.Upgrade(writer, request, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	r.ws.TakeConnection(conn)
+	r.ws.TakeConnection(username, conn)
 }
--- server/ws/server.go
+++ server/ws/server.go
@@ -7,42 +7,48 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type connection struct {
+	conn *websocket.Conn
+	user string
+}
+
 type Server struct {
-	connections []*websocket.Conn
+	connections []connection
 }
 
 func NewServer() *Server {
-	return &Server{connections: make([]*websocket.Conn, 0)}
+	return &Server{connections: make([]connection, 0)}
 }
 
 func (s *Server) Close() {
 	for _, conn := range s.connections {
-		conn.Close()
+		conn.conn.Close()
 	}
 }
 
-func (s *Server) TakeConnection(conn *websocket.Conn) {
-	s.connections = append(s.connections, conn)
-	go s.handleConnection(conn)
+func (s *Server) TakeConnection(username string, conn *websocket.Conn) {
+	c := connection{conn: conn, user: username}
+	s.connections = append(s.connections, c)
+	go s.handleConnection(c)
 }
 
-func (s *Server) handleConnection(conn *websocket.Conn) {
+func (s *Server) handleConnection(c connection) {
 	for {
-		_, message, err := conn.ReadMessage()
+		_, message, err := c.conn.ReadMessage()
 		if err != nil {
-			log.Println("failed to read message ", err)
+			log.Printf("failed to read message for user '%s' with error '%s' \n", c.user, err)
 			break
 		}
 
 		var event Event
 		if err := json.Unmarshal(message, &event); err != nil {
-			log.Println("failed to unmarshal event ", err)
+			log.Printf("failed to unmarshal event %s \n", err.Error())
 			continue
 		}
 
 		switch event.EventType {
 		case "task_created":
-			log.Println("received task_created event")
+			log.Println("received task_created event from user ", c.user)
 			taskCreated, err := event.AsEventTaskCreated()
 			if err != nil {
 				log.Println("failed to parse task_created event ", err, " ", string(message))
@@ -51,7 +57,9 @@ func (s *Server) handleConnection(conn *websocket.Conn) {
 
 			log.Println(taskCreated)
 
-			s.broadcast(taskCreated)
+			s.store.tasks.Add(task)
+
+			s.broadcast(taskCreated, c)
 		default:
 			log.Println("unknown event type ", event.EventType)
 			log.Println(string(message))
@@ -59,9 +67,13 @@ func (s *Server) handleConnection(conn *websocket.Conn) {
 	}
 }
 
-func (s *Server) broadcast(taskCreated EventTaskCreated) {
+func (s *Server) broadcast(taskCreated EventTaskCreated, broadcaster connection) {
 	for _, conn := range s.connections {
-		if err := conn.WriteJSON(taskCreated); err != nil {
+		if conn.user == broadcaster.user {
+			continue
+		}
+		log.Printf("broadcasting to user '%s' \n", conn.user)
+		if err := conn.conn.WriteJSON(taskCreated); err != nil {
 			log.Println(err)
 		}
 	}
